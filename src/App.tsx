b7d7291f08e53c4647358ab4089292a766021d72
jung juwon <02juwon@naver.com>
2026-03-20 23:26:18 +0900

    feat : 초기 환경 구축 및 스켈레톤 UI 구현

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// src/App.tsx
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh', 
+      backgroundColor: '#f0f0f0' 
+    }}>
+      {/* PM이 요청한 바로 그 정사각형 */}
+      <div style={{ 
+        width: '200px', 
+        height: '200px', 
+        backgroundColor: '#646cff' 
+      }}></div>
+    </div>
+  );
+}
+
+export default App;
